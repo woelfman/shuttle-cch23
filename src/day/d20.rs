ea87488ea03aae1ad979df7b05baed90c2d114ac
Matt Woelfel <matt@woelfware.com>
2023-12-21 10:54:45 -0600

    Add day 20 part 2

--- src/day/d20.rs
+++ src/day/d20.rs
@@ -2,37 +2,22 @@ use std::io::Cursor;
 
 use axum::{
     body::Bytes,
-    http::{HeaderMap, StatusCode},
+    http::StatusCode,
     response::IntoResponse,
     routing::post,
     Router,
 };
 use tar::Archive;
+use tokio::process::Command;
 
 pub fn get_routes() -> Router {
     Router::new()
         .route("/20/archive_files", post(archive_files))
         .route("/20/archive_files_size", post(archive_files_size))
+        .route("/20/cookie", post(cookie))
 }
 
-async fn archive_files(
-    headers: HeaderMap,
-    body: Bytes,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    if let Some(ct) = headers.get("Content-Type") {
-        if ct != "application/x-tar" {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                "Expected Content-Type: application/x-tar".to_string(),
-            ));
-        }
-    } else {
-        return Err((
-            StatusCode::BAD_REQUEST,
-            "Expected Content-Type: application/x-tar".to_string(),
-        ));
-    }
-
+async fn archive_files(body: Bytes) -> impl IntoResponse {
     let mut archive = Archive::new(Cursor::new(body));
 
     let files = archive
@@ -40,32 +25,67 @@ async fn archive_files(
         .map(|entries| entries.count())
         .unwrap_or(0);
 
-    Ok((StatusCode::OK, files.to_string()))
+    files.to_string()
 }
 
-async fn archive_files_size(
-    headers: HeaderMap,
-    body: Bytes,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    if let Some(ct) = headers.get("Content-Type") {
-        if ct != "application/x-tar" {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                "Expected Content-Type: application/x-tar".to_string(),
-            ));
-        }
-    } else {
-        return Err((
-            StatusCode::BAD_REQUEST,
-            "Expected Content-Type: application/x-tar".to_string(),
-        ));
-    }
-
+async fn archive_files_size(body: Bytes) -> impl IntoResponse {
     let mut archive = Archive::new(Cursor::new(body));
 
     let files_size = archive.entries().unwrap().fold(0u64, |acc, entry| {
         acc + entry.unwrap().header().size().unwrap()
     });
 
-    Ok((StatusCode::OK, files_size.to_string()))
+    files_size.to_string()
+}
+
+async fn cookie(body: Bytes) -> Result<impl IntoResponse, impl IntoResponse> {
+    // Extract the archive to a temporary directory
+
+    let dst =
+        tempfile::tempdir().map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    let mut archive = Archive::new(Cursor::new(body));
+
+    archive
+        .unpack(&dst)
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    // Use `git` to find commit
+
+    let mut command = Command::new("git");
+    let output = command
+        .args(["log", "christmas", "-p", "--", "*santa.txt"])
+        .current_dir(&dst)
+        .output()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
+        .stdout;
+    let output = String::from_utf8(output)
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    for commit in output.split("commit ") {
+        for line in commit.lines() {
+            if line.starts_with('+') && line.contains("COOKIE") {
+
+                for line in commit.lines() {
+                    if line.starts_with("Author: ") {
+                        let author = line
+                            .strip_prefix("Author: ")
+                            .unwrap()
+                            .split(" <")
+                            .next()
+                            .unwrap();
+                        let hash = commit.lines().next().unwrap();
+
+                        return Ok(format!("{author} {hash}"));
+                    }
+                }
+            }
+        }
+    }
+
+    Err((
+        StatusCode::UNPROCESSABLE_ENTITY,
+        "commit not found".to_string(),
+    ))
 }
