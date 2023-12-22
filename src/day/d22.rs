dfb8a1591dab8bc4786e4172392edb9ce8e52e34
Matt Woelfel <matt@woelfware.com>
2023-12-22 17:42:12 -0600

    Add day 22 part 2

--- src/day/d22.rs
+++ src/day/d22.rs
@@ -1,9 +1,11 @@
 use axum::{routing::post, Router};
 use itertools::Itertools;
+use petgraph::{algo::astar, stable_graph::NodeIndex, Graph};
 
 pub fn get_routes() -> Router {
     Router::new()
         .route("/22/integers", post(integers))
+        .route("/22/rocket", post(rocket))
 }
 
 async fn integers(body: String) -> String {
@@ -21,4 +23,82 @@ async fn integers(body: String) -> String {
     }
 
     "".to_string()
+}
+
+struct Coordinate {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Coordinate {
+    pub fn distance(&self, b: &Self) -> f32 {
+        ((self.x as f32 - b.x as f32).powi(2)
+            + (self.y as f32 - b.y as f32).powi(2)
+            + (self.z as f32 - b.z as f32).powi(2))
+        .sqrt()
+    }
+}
+
+struct Portal {
+    entrance: u8,
+    exit: u8,
+}
+
+async fn rocket(body: String) -> String {
+    let mut graph = Graph::new();
+    let mut iter = body.lines();
+    let n = iter.next().and_then(|n| n.parse::<u8>().ok()).unwrap_or(0);
+    for _ in 0..n {
+        let coordinate: Coordinate = iter
+            .next()
+            .map(|s| s.split_whitespace().filter_map(|s| s.parse::<i32>().ok()))
+            .map(|mut v| Coordinate {
+                x: v.next().unwrap(),
+                y: v.next().unwrap(),
+                z: v.next().unwrap(),
+            })
+            .unwrap();
+        graph.add_node(coordinate);
+    }
+    let k = iter.next().and_then(|k| k.parse::<u8>().ok()).unwrap_or(0);
+    for _ in 0..k {
+        let portal: Portal = iter
+            .next()
+            .map(|s| s.split_whitespace().filter_map(|s| s.parse::<u8>().ok()))
+            .map(|mut v| Portal {
+                entrance: v.next().unwrap(),
+                exit: v.next().unwrap(),
+            })
+            .unwrap();
+        graph.add_edge(
+            NodeIndex::new(portal.entrance.into()),
+            NodeIndex::new(portal.exit.into()),
+            (),
+        );
+    }
+
+    let end = NodeIndex::new(graph.node_count() - 1);
+    let path = astar(
+        &graph,
+        NodeIndex::new(0),
+        |finish| finish == end,
+        |_| 1,
+        |_| 0,
+    )
+    .unwrap();
+
+    let distance = path
+        .1
+        .iter()
+        .tuple_windows::<(_, _)>()
+        .map(|(a, b)| {
+            (
+                graph.node_weight(*a).unwrap(),
+                graph.node_weight(*b).unwrap(),
+            )
+        })
+        .fold(0.0f32, |acc, (a, b)| acc + a.distance(b));
+
+    format!("{} {:.3}", path.0, distance)
 }
