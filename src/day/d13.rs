bb5d5d70265ff47b16b3703328f09edd9ae63106
Matt Woelfel <matt@woelfware.com>
2024-02-23 18:15:47 -0600

    Add sea-query for building sqlx queries

--- src/day/d13.rs
+++ src/day/d13.rs
@@ -1,48 +1,68 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
-use serde::{Deserialize, Serialize};
+use sea_query::{Alias, ColumnDef, Expr, Iden, Order, PostgresQueryBuilder, Query, Table};
+use sea_query_binder::SqlxBinder;
+use serde::Deserialize;
 use sqlx::FromRow;
 
 use crate::AppState;
 
-#[derive(Serialize, FromRow)]
-struct Task1 {
-    pub id: i32,
-    pub num: i32,
-}
+#[derive(FromRow)]
+struct Task1(i32);
 
 pub async fn sql(State(state): State<AppState>) -> Result<impl IntoResponse, impl IntoResponse> {
-    match sqlx::query!("SELECT 20231213 as \"i32\"")
+    let (sql, values) = Query::select()
+        .expr(Expr::val(20231213))
+        .build_sqlx(PostgresQueryBuilder);
+
+    match sqlx::query_as_with::<_, Task1, _>(&sql, values)
         .fetch_one(&state.pool)
         .await
     {
-        Ok(record) => Ok((StatusCode::OK, record.i32.unwrap().to_string())),
+        Ok(task) => Ok((StatusCode::OK, task.0.to_string())),
         Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
     }
 }
 
+#[derive(Iden)]
+enum Orders {
+    Table,
+    Id,
+    RegionId,
+    GiftName,
+    Quantity,
+}
+
 pub async fn reset(State(state): State<AppState>) -> Result<StatusCode, StatusCode> {
-    sqlx::query!("DROP TABLE IF EXISTS orders")
+    let query = Table::drop()
+        .table(Orders::Table)
+        .if_exists()
+        .build(PostgresQueryBuilder);
+
+    sqlx::query(&query)
         .execute(&state.pool)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    sqlx::query!(
-        "CREATE TABLE orders (
-        id INT PRIMARY KEY,
-        region_id INT,
-        gift_name VARCHAR(50),
-        quantity INT
-    )"
-    )
-    .execute(&state.pool)
-    .await
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let query = Table::create()
+        .table(Orders::Table)
+        .if_not_exists()
+        .col(ColumnDef::new(Orders::Id).integer().primary_key())
+        .col(ColumnDef::new(Orders::RegionId).integer())
+        .col(ColumnDef::new(Orders::GiftName).string_len(50))
+        .col(ColumnDef::new(Orders::Quantity).integer())
+        .build(PostgresQueryBuilder);
+
+    sqlx::query(&query)
+        .execute(&state.pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(StatusCode::OK)
 }
 
 #[derive(Deserialize)]
-pub struct Order {
+pub struct OrderStruct {
     id: i32,
     region_id: i32,
     gift_name: String,
@@ -51,56 +71,86 @@ pub struct Order {
 
 pub async fn orders(
     State(state): State<AppState>,
-    Json(orders): Json<Vec<Order>>,
+    Json(orders): Json<Vec<OrderStruct>>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    for Order {
+    for OrderStruct {
         id,
         region_id,
         gift_name,
         quantity,
     } in orders
     {
-        sqlx::query!(
-            "INSERT INTO orders VALUES ($1, $2, $3, $4)",
-            id,
-            region_id,
-            gift_name,
-            quantity
-        )
-        .execute(&state.pool)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+        let (sql, values) = Query::insert()
+            .into_table(Orders::Table)
+            .columns([
+                Orders::Id,
+                Orders::RegionId,
+                Orders::GiftName,
+                Orders::Quantity,
+            ])
+            .values_panic([
+                id.into(),
+                region_id.into(),
+                gift_name.clone().into(),
+                quantity.into(),
+            ])
+            .returning_col(Orders::Id)
+            .build_sqlx(PostgresQueryBuilder);
+
+        let _row = sqlx::query_with(&sql, values)
+            .fetch_one(&state.pool)
+            .await
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
     }
 
     Ok(StatusCode::OK)
 }
 
+#[derive(FromRow)]
+struct Task2(i64);
+
 pub async fn orders_total(
     State(state): State<AppState>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let total = sqlx::query!("select sum(quantity) as \"i64!\" from orders")
+    let (sql, values) = Query::select()
+        .expr_as(Expr::col(Orders::Quantity).sum(), Alias::new("i64"))
+        .from(Orders::Table)
+        .build_sqlx(PostgresQueryBuilder);
+
+    let row = sqlx::query_as_with::<_, Task2, _>(&sql, values)
         .fetch_one(&state.pool)
         .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
-        .i64;
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    Ok(format!("{{\"total\":{total}}}"))
+    Ok(format!("{{\"total\":{}}}", row.0))
+}
+
+#[derive(FromRow)]
+struct OrdersPopular {
+    sq: i64,
+    gift_name: String,
 }
 
 pub async fn orders_popular(
     State(state): State<AppState>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let gifts = sqlx::query!(
-        "select sum(quantity) as \"sq!\", gift_name as \"gift_name!\"
-        from orders group by gift_name order by \"sq!\" desc"
-    )
-    .fetch_all(&state.pool)
-    .await
-    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-    match gifts.len() {
+    let (sql, values) = Query::select()
+        .expr_as(Expr::col(Orders::Quantity).sum(), Alias::new("sq"))
+        .column(Orders::GiftName)
+        .from(Orders::Table)
+        .group_by_col(Orders::GiftName)
+        .order_by(Alias::new("sq"), Order::Desc)
+        .build_sqlx(PostgresQueryBuilder);
+
+    let rows = sqlx::query_as_with::<_, OrdersPopular, _>(&sql, values)
+        .fetch_all(&state.pool)
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    match rows.len() {
         0 => Ok("{\"popular\":null}".to_string()),
-        1 => Ok(format!("{{\"popular\":\"{}\"}}", gifts[0].gift_name)),
-        _ if gifts[0].sq == gifts[1].sq => Ok("{\"popular\":null}".to_string()),
-        _ => Ok(format!("{{\"popular\":\"{}\"}}", gifts[0].gift_name)),
+        1 => Ok(format!("{{\"popular\":\"{}\"}}", rows[0].gift_name)),
+        _ if rows[0].sq == rows[1].sq => Ok("{\"popular\":null}".to_string()),
+        _ => Ok(format!("{{\"popular\":\"{}\"}}", rows[0].gift_name)),
     }
 }
--- src/day/d18.rs
+++ src/day/d18.rs
@@ -5,41 +5,89 @@ use axum::{
     Json,
 };
 
+use sea_query::{Alias, ColumnDef, Expr, Iden, Order, PostgresQueryBuilder, Query, Table};
+use sea_query_binder::SqlxBinder;
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
 use crate::AppState;
 
 pub async fn reset(State(state): State<AppState>) -> Result<StatusCode, StatusCode> {
-    sqlx::query!("DROP TABLE IF EXISTS regions")
+    let query = Table::drop()
+        .table(Regions::Table)
+        .if_exists()
+        .build(PostgresQueryBuilder);
+
+    sqlx::query(&query)
         .execute(&state.pool)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    sqlx::query!("DROP TABLE IF EXISTS orders")
+
+    let query = Table::create()
+        .table(Regions::Table)
+        .if_not_exists()
+        .col(
+            ColumnDef::new(Regions::Id)
+                .integer()
+                .not_null()
+                .auto_increment()
+                .primary_key(),
+        )
+        .col(ColumnDef::new(Regions::Name).string_len(50))
+        .build(PostgresQueryBuilder);
+
+    sqlx::query(&query)
         .execute(&state.pool)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    sqlx::query!(
-        "CREATE TABLE regions (
-        id INT PRIMARY KEY,
-        name VARCHAR(50)
-    )"
-    )
-    .execute(&state.pool)
-    .await
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    sqlx::query!(
-        "CREATE TABLE orders (
-        id INT PRIMARY KEY,
-        region_id INT,
-        gift_name VARCHAR(50),
-        quantity INT
-    )"
-    )
-    .execute(&state.pool)
-    .await
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let query = Table::drop()
+        .table(Orders::Table)
+        .if_exists()
+        .build(PostgresQueryBuilder);
+
+    sqlx::query(&query)
+        .execute(&state.pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let query = Table::create()
+        .table(Orders::Table)
+        .if_not_exists()
+        .col(
+            ColumnDef::new(Orders::Id)
+                .integer()
+                .not_null()
+                .auto_increment()
+                .primary_key(),
+        )
+        .col(ColumnDef::new(Orders::RegionId).integer())
+        .col(ColumnDef::new(Orders::GiftName).string_len(50))
+        .col(ColumnDef::new(Orders::Quantity).integer())
+        .build(PostgresQueryBuilder);
+
+    sqlx::query(&query)
+        .execute(&state.pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(StatusCode::OK)
+}
+
+#[derive(Iden)]
+enum Regions {
+    Id,
+    Name,
+    Table,
+}
+
+#[derive(Iden)]
+enum Orders {
+    GiftName,
+    Id,
+    Quantity,
+    RegionId,
+    Table,
 }
 
 #[derive(Deserialize)]
@@ -53,8 +101,15 @@ pub async fn regions(
     Json(regions): Json<Vec<Region>>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     for Region { id, name } in regions {
-        sqlx::query!("INSERT INTO regions VALUES ($1, $2)", id, name)
-            .execute(&state.pool)
+        let (sql, values) = Query::insert()
+            .into_table(Regions::Table)
+            .columns([Regions::Id, Regions::Name])
+            .values_panic([id.into(), name.into()])
+            .returning_col(Regions::Name)
+            .build_sqlx(PostgresQueryBuilder);
+
+        let _row = sqlx::query_with(&sql, values)
+            .fetch_one(&state.pool)
             .await
             .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
     }
@@ -62,75 +117,95 @@ pub async fn regions(
     Ok(StatusCode::OK)
 }
 
-#[derive(Serialize)]
+#[derive(FromRow, Serialize)]
 struct Total {
-    region: String,
-    total: i64,
+    #[serde(rename(serialize = "region"))]
+    name: String,
+    total: Option<i64>,
 }
 
 pub async fn regions_total(
     State(state): State<AppState>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let mut totals = Vec::new();
-    let regions = sqlx::query!(
-        "SELECT regions.name, SUM(orders.quantity) AS total
-        FROM regions
-        LEFT JOIN orders ON orders.region_id = regions.id
-        GROUP BY regions.name
-        ORDER BY regions.name;"
-    )
-    .fetch_all(&state.pool)
-    .await
-    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let (sql, values) = Query::select()
+        .column(Regions::Name)
+        .expr_as(
+            Expr::col((Orders::Table, Orders::Quantity)).sum(),
+            Alias::new("total"),
+        )
+        .from(Regions::Table)
+        .left_join(
+            Orders::Table,
+            Expr::col((Orders::Table, Orders::RegionId)).equals((Regions::Table, Regions::Id)),
+        )
+        .group_by_col((Regions::Table, Regions::Name))
+        .order_by((Regions::Table, Regions::Name), Order::Asc)
+        .build_sqlx(PostgresQueryBuilder);
 
-    for record in regions.iter().filter(|r| r.total.is_some()) {
-        if let (Some(region), Some(total)) = (&record.name, record.total) {
-            totals.push(Total {
-                region: region.clone(),
-                total,
-            });
-        }
-    }
+    let rows: Vec<Total> = sqlx::query_as_with::<_, Total, _>(&sql, values)
+        .fetch_all(&state.pool)
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
+        .into_iter()
+        .filter(|row| row.total.is_some())
+        .collect();
 
-    Ok(Json(totals))
+    Ok(Json(rows))
 }
 
-#[derive(Serialize)]
+#[derive(FromRow, Serialize)]
 struct TopGifts {
     region: String,
     top_gifts: Vec<String>,
 }
 
+#[derive(FromRow)]
+struct RegionStruct {
+    id: i32,
+    name: String,
+}
+
+#[derive(FromRow)]
+struct TopGiftsStruct(String);
+
 pub async fn top_list(
     State(state): State<AppState>,
-    Path(number): Path<i64>,
+    Path(number): Path<u64>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let mut top_list = Vec::new();
+    let mut top_list: Vec<TopGifts> = Vec::new();
 
-    let regions = sqlx::query!("SELECT id, name FROM regions ORDER BY name")
+    let (sql, values) = Query::select()
+        .column(Regions::Id)
+        .column(Regions::Name)
+        .from(Regions::Table)
+        .order_by(Regions::Name, Order::Asc)
+        .build_sqlx(PostgresQueryBuilder);
+
+    let regions = sqlx::query_as_with::<_, RegionStruct, _>(&sql, values)
         .fetch_all(&state.pool)
         .await
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
     for region in regions {
-        let top_gifts = sqlx::query!(
-            "SELECT gift_name FROM orders
-            WHERE region_id = $1
-            GROUP BY gift_name
-            ORDER BY SUM(quantity)
-            DESC LIMIT $2",
-            region.id,
-            number
-        )
-        .fetch_all(&state.pool)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
-        .into_iter()
-        .filter_map(|r| r.gift_name)
-        .collect();
+        let (sql, values) = Query::select()
+            .column(Orders::GiftName)
+            .from(Orders::Table)
+            .and_where(Expr::col(Orders::RegionId).eq(region.id))
+            .limit(number)
+            .group_by_col(Orders::GiftName)
+            .order_by_expr(Expr::col(Orders::Quantity).sum(), Order::Desc)
+            .build_sqlx(PostgresQueryBuilder);
+
+        let top_gifts = sqlx::query_as_with::<_, TopGiftsStruct, _>(&sql, values)
+            .fetch_all(&state.pool)
+            .await
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
+            .into_iter()
+            .map(|tg| tg.0)
+            .collect();
 
         top_list.push(TopGifts {
-            region: region.name.unwrap(),
+            region: region.name,
             top_gifts,
         });
     }
